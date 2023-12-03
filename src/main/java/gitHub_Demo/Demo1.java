ca4d8027e79574ed5e8d5aadfa86f31e79483a41
nirjalapatel <nirjala221503@gmail.com>
2023-12-03 02:28:17 -0800

    first commit

--- src/main/java/gitHub_Demo/Demo1.java
+++ src/main/java/gitHub_Demo/Demo1.java
@@ -1,9 +1,8 @@
 package gitHub_Demo;
 
-public class GitTest {
-	
+public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println("hii-----------");
+		System.out.println("User one");
 	}
 
 }
